f363122404ac92533df7df326d2590cb0a9e846b
BryanGK <bryangordonkrauss@gmail.com>
2021-03-24 10:13:01 -0700

    Basic Home page

--- API/client/src/components/Home.js
+++ API/client/src/components/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
 
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <div className="jumbotron">
+                <h1 className="display-4">Welcome to Twit-stravaganza!</h1>
+                <p className="lead">A twitter API showcase app.</p>
+                <hr className="my-4"></hr>
+                <p>Come on in and search for your favourite tweeters or get random tweets that will entice the soul and excite the mind.</p>
+                <p className="lead">
+                    <Router>
+                        <Link className="btn btn-primary btn-lg" role="button" to="/search">Enter</Link>
+                    </Router>
+                </p>
+            </div>
         </div>
     );
 }
