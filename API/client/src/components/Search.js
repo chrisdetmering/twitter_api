9b6c93453aed08e9a386f96c06ed5b5ccce5513e
BryanGK <bryangordonkrauss@gmail.com>
2021-03-25 08:23:58 -0700

    Search bar added

--- API/client/src/components/Search.js
+++ API/client/src/components/Search.js
@@ -3,7 +3,13 @@ import React from 'react';
 function Search() {
     return (
         <div>
-            <h1>Search</h1>
+            <h1>Search for your favourite Tweeters</h1>
+            <div className="container">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Twitter username" aria-label="username" aria-describedby="basic-addon2"></input>
+                    <span class="input-group-text" id="basic-addon2">Search</span>
+                </div>
+            </div>
         </div>
     );
 }
