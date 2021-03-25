d6b96b5ff4fdbf06d8fc07b58124778cc8d4fe30
BryanGK <bryangordonkrauss@gmail.com>
2021-03-25 09:25:05 -0700

    Functioning navbar

--- API/client/src/App.js
+++ API/client/src/App.js
@@ -8,14 +8,17 @@ function App() {
     return (
         <Router>
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid">
-                        <Link className="nav-link" to="/">Home</Link>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <div className="container-fluid">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <span className="navbar-brand" href="/">Twitter App</span>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNav">
                             <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/">Home</Link>
+                                </li>
                                 <li className="nav-item">
                                     <Link className="nav-link" to="/search">Search</Link>
                                 </li>
@@ -24,8 +27,8 @@ function App() {
                                 </li>
                             </ul>
                         </div>
-                    </div>
-                </nav>
+                    </nav>
+                </div>
                 <Switch>
                     <Route path="/search">
                         <Search />
