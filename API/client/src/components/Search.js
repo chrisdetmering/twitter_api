11eca45460edf8c8b0a2c5f99b64d16f7e9e4040
BryanGK <bryangordonkrauss@gmail.com>
2021-03-25 09:52:19 -0700

    Card up on search

--- API/client/src/components/Search.js
+++ API/client/src/components/Search.js
@@ -13,6 +13,19 @@ function Search() {
                     </div>
                 </div>
             </div>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
