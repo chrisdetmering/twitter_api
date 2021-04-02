35c08d493d50573533fd8b2ca0382c391f56b5ed
BryanGK <bryangordonkrauss@gmail.com>
2021-04-02 08:16:56 -0700

    tweets on page

--- API/client/src/components/DisplayCard.js
+++ API/client/src/components/DisplayCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function DisplayCard({ twitterData }) {
 
-    return twitterData.map(item => {
+    return twitterData.statuses.map(item => {
         return (
             <div className="row justify-content-center" key={Math.random()}>
                 <div className="card" id="search-card">
                     <div className="card-body">
-                        <h5 className="card-title">{item.authorId}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">{item.id}</h6>
+                        <h5 className="card-title">{item.user.name}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{item.user.scree_name}</h6>
                         <p className="card-text">{item.text}</p>
                         <a href="/" className="card-link">Card link</a>
                         <a href="/" className="card-link">Another link</a>
